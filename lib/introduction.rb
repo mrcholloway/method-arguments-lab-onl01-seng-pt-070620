7d47e06b4b14fbcaa13fa00b88a0847990b0171c
Christopher Holloway <mrcholloway@yahoo.com>
2020-07-11 01:09:31 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,6 +2,6 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-def introduction_with_name_and_language(name, language)
+def introduction(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
